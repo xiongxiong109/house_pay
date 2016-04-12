1156c10d4a0f3a5bf04dd7443790e53101c7b8b5
xiongxiong109 <444122662@qq.com>
2016-04-12 22:19:11 +0800

    页面更新

--- app.js
+++ app.js
@@ -12,7 +12,10 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
+// 输出不压缩的html
+app.locals.pretty = true;
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,10 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-	var preStr = require('../data/lilv');
+	var lilv = require('../data/lilv');
 	var json = {
 		title: '房贷计算器',
-		lilv_array: preStr, // 利率数组
+		lilv: lilv, //利率数据
 		loanradiotype: ['商业贷款','公积金贷款','组合型贷款'] // 贷款类别
 	};
 
